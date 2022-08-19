3acd1618848a3391e2c5a9714d35647cddc84d44
samzakovich <samzakovich@gmail.com>
2022-08-19 10:59:06 -0500

    add table.py file

---
+++ table.py
@@ -0,0 +1,8 @@
+def makeTable(players):
+    table = ""
+    return table
+
+if __name__ == '__main__':
+    with open('players/master.txt') as f:
+        lines = f.readlines()
+    makeTable(lines)
